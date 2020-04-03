cbcf29785804bd60d467d877d3c003025fcda549
Neutrovertido <russellcode.james@gmail.com>
2020-04-03 13:10:07 -0600

    Dark mode (beta) added

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// About function
+const ab =  document.getElementById("about");
+
+ab.onclick = () => {
+    alert("Webpad b0.1\nMade by Neutrovertido - 2020");
+}
+
+// Theme function
+const th = document.getElementById("theme");
+const body = document.body;
+let currentTheme = body.classList;
+let cacheTheme = localStorage.getItem("webth");
+
+if (cacheTheme === "dark") {
+    body.classList.replace("light","dark");
+}
+
+th.onclick = () => {
+    if (body.classList.contains("light")) {
+        body.classList.replace("light","dark");
+        localStorage.setItem("webth", "dark");
+    } else {
+        body.classList.replace("dark","light");
+        localStorage.setItem("webth", "light");
+    }
+}
