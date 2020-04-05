e0bdd54250d6bf36910327dea10565ca52ee6fe7
Neutrovertido <russellcode.james@gmail.com>
2020-04-05 01:53:09 -0600

    Save, Bugs--, Prettier

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // About function
-const ab =  document.getElementById("about");
+const ab = document.getElementById("about");
 
 ab.onclick = () => {
     alert("Webpad b0.1\nMade by Neutrovertido - 2020");
@@ -12,15 +12,22 @@ let currentTheme = body.classList;
 let cacheTheme = localStorage.getItem("webth");
 
 if (cacheTheme === "dark") {
-    body.classList.replace("light","dark");
+    body.classList.replace("light", "dark");
 }
 
 th.onclick = () => {
     if (body.classList.contains("light")) {
-        body.classList.replace("light","dark");
+        body.classList.replace("light", "dark");
         localStorage.setItem("webth", "dark");
     } else {
-        body.classList.replace("dark","light");
+        body.classList.replace("dark", "light");
         localStorage.setItem("webth", "light");
     }
+}
+
+// Save function
+const sb = document.getElementById("ward");
+
+sb.onclick = () => {
+    alert("Someday it will eventualy work");
 }
